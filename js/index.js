5022203dfbb118474e26e948ae36de55b3a3b7ed
AnaysJesu <anaplatano.23@gmail.com>
2025-04-22 22:39:08 -0400

    cambios visuales

--- js/index.js
+++ js/index.js
@@ -1,44 +1,44 @@
 
-function mostrarHora(){
+function mostrarHora() {
     const ahora = new Date();
-    const fechaHora = ahora.toLocaleString('es-CL',{
+    const fechaHora = ahora.toLocaleString('es-CL', {
         hour12: false
     });
     document.getElementById('fechaHora').textContent = fechaHora;
 }
 setInterval(mostrarHora, 1000);
-
-window.onload = function(){
-    let Noticias = JSON.parse(localStorage.getItem("noticias"))||[];
+window.onload = function () {
+    let Noticias = JSON.parse(localStorage.getItem("noticias")) || [];
     let contadorGeneral = 0;
     let contadorDeporte = 0;
     let contadorNegocio = 0;
-
-    Noticias.forEach(noticia =>{
-        const fila = document.createElement("tr");
-        fila.innerHTML = `
-            <th><h3>${noticia.titulo}</h3></th>
-            <th><h3>${noticia.tipo}</h3></th>
-            <th>
-                <img src="${noticia.link}" width="200"><br>
-                <p>${noticia.descripcion}</p>
-            </th>
+    Noticias.forEach(noticia => {
+        const col = document.createElement("div");
+        col.className = "col-md-4";
+        col.innerHTML = `
+            <div class="card h-100">
+                <img src="${noticia.link}" class="card-img-top" alt="${noticia.titulo}">
+                <div class="card-body">
+                    <h3 class="card-title">${noticia.titulo}</h3>
+                    <h3>${noticia.tipo}</h3>
+                    <p class="card-text">${noticia.descripcion}</p>
+                </div>
+            </div>
         `;
-        if (noticia.seccion === "general"){
-            document.querySelector(".TablaNoticiaGeneral").appendChild(fila);
+        if (noticia.seccion === "general") {
+            let seccion = document.querySelector("#SeccionGeneral").parentElement;
+            let generalContainer = seccion.querySelector(".row") || document.createElement("div");
+            generalContainer.className = "row g-4";
+            generalContainer.appendChild(col);
+            seccion.appendChild(generalContainer);
             contadorGeneral++;
-        } else if (noticia.seccion === "deporte"){
-            document.getElementById("SeccionDeDeportes").appendChild(fila);
+        } else if (noticia.seccion === "deporte") {
+            let deporteContainer = document.querySelectorAll("#SeccionDeDeportes .row")[0];
+            deporteContainer.appendChild(col);
             contadorDeporte++;
-        } else if (noticia.seccion === "negocio"){
-            const contenedor = document.querySelector("section:nth-of-type(3)");
-            const div = document.createElement("div");
-            div.innerHTML = `
-                <h3>${noticia.titulo}</h3>
-                <img src="${noticia.link}" width="200"><br>
-                <p>${noticia.descripcion}</p>
-            `;
-            contenedor.appendChild(div);
+        } else if (noticia.seccion === "negocio") {
+            let negocioContainer = document.querySelector("#negocios .row");
+            negocioContainer.appendChild(col);
             contadorNegocio++;
         }
     });
@@ -47,7 +47,7 @@ window.onload = function(){
     document.getElementById("contador-negocio").textContent = contadorNegocio;
     document.getElementById("btnNuevaNoticia").addEventListener("click", mostrarNuevoArticulo);
 };
-function mostrarNuevoArticulo(){
-    window.location.href = "mantenedorArticulo.html";
+function mostrarNuevoArticulo() {
+    window.location.href = "mantenedorArticulo.html"; 
 }
 
